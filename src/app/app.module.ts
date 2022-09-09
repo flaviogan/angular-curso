5bcff7fdb1e926dd993d31a3f7a58c58fa0bc10a
Flavio Ganci <flavio.ganci@gmail.com>
2022-09-09 14:35:02 -0300

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { contadorComponent } from './Contador/contador/contador.component';
+import { contadorModule } from './Contador/contador.module';
 import { HeroesMudule } from './Heroes/Heroes.module';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    contadorComponent,
+    AppComponent   
 
   ],
   imports: [
     BrowserModule,
-    HeroesMudule
+    HeroesMudule,
+    contadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
